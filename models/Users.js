36d3ab2a2c028334185fb88d6fb411d23d85513c
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-05 20:19:51 -0700

    updated

---
+++ models/Users.js
@@ -0,0 +1,57 @@
+"use strict";
+const {DataTypes} = require("sequelize");
+module.exports = (sequelize, DataTypes) =>{
+    return  sequelize.define('user', {
+        id:{
+            type:DataTypes.UUID,
+            primaryKey:true,
+            allowNull:false,
+        },
+        first_name:{
+            type:DataTypes.STRING,
+            isAlphanumeric:true, //cant put special characters in the name.
+            required: true,
+            allowNull: false,
+        },
+        last_name:{
+            type:DataTypes.STRING,
+            required: true,
+            allowNull: false,
+        },
+        username:{
+            type:DataTypes.STRING,
+            required:true,
+            allowNull:false,
+            len:[8,20],
+        },
+        password:{
+            type:DataTypes.STRING,
+            required:true,
+            allowNull:false,
+            len:[8,20],
+        },
+        email:{
+            type:DataTypes.STRING,
+            required:true,
+            allowNull:false,
+            len:[7,100],
+            isEmail:true,
+        },
+        // permission_id:{
+        //     //fk in permission table.
+        //   type:DataTypes.INTEGER,
+        //   allowNull:false,
+        //   required:true,
+        // },
+        updated_at:{
+            type:DataTypes.DATE,
+        },
+        deleted_at:{
+            type:DataTypes.DATE,
+        },
+    },
+    {
+        underscored:true,
+        paranoid:true,
+    });
+}
