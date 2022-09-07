1bbeb22ab7d2b268452f426726e86668ff381aed
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-07 04:25:39 -0700

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) =>{
     return  sequelize.define('user', {
         id:{
             type:DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
             primaryKey:true,
             allowNull:false,
         },
