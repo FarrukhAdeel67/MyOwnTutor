27a222b620a07b10f78afceaab43bb46bb01b2dc
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-09 16:32:06 -0700

    Update user_identification.js

--- middleware/user_identification.js
+++ middleware/user_identification.js
@@ -1,16 +1,13 @@
 const User = require("../Services/Users/User_Service");
-module.exports = (req,res, next)=>{
-    const user_id = req.user?req.user:null;
-    if(user_id){
-        User.FindUser(user_id)
-            .then(user=>{
-                if(user){
-                    req.user_permission = user.user_permission;
-                }
-            })
-    }
-    else{
-        next();
-    }
-
-}
+module.exports = (req, res, next) => {
+  const user_id = req.user ? req.user : null;
+  if (user_id) {
+    User.FindUser(user_id).then((user) => {
+      if (user) {
+        req.user_permission = user.user_permission;
+      }
+    });
+  } else {
+    next();
+  }
+};
