b641acf58193a424e52cc29da6b14d1d3ca15f3d
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-09 00:33:42 -0700

    Update User_DB.js

--- Services/Users/User_DB.js
+++ Services/Users/User_DB.js
@@ -1,4 +1,6 @@
 const db = require("../../models");
+const {formatArgs} = require("debug");
+
 /*
 Functions checks if username already exists on database.
 Returns user if username already taken, false otherwise,
@@ -16,8 +18,26 @@ async  function UsernameExists(username){
     }
     return null;
 }
-// Function checks if email already exists in database
-//returns user if email is already taken ,false otherwise,
+
+/*
+* Function for finding a user based on their user id.
+* @args
+-user_id:string,
+*
+* */
+async function FindUserByUserId(user_id){
+    if(!user_id) throw new Error("Invalid argument: user_id");
+    const user = await db.user.findOne({
+        where:{id:user_id},
+    })
+    if(user){
+        return user;
+    }
+    return null;
+}
+
+/*Function checks if email already exists in database
+returns user if email is already taken ,false otherwise,*/
 async function  EmailExists(email){
     if(email===null || email=== undefined){
         throw new Error('No Email  was passed as an argument')
@@ -31,13 +51,16 @@ async function  EmailExists(email){
     }
     return null;
 }
+
 async function CreateUser(args){
     if(!args.username) throw new Error('Invalid argument: username');
     if(!args.first_name) throw new Error('Invalid argument: first_name');
     if(!args.last_name) throw new Error('Invalid argument: last_name');
     if(!args.password) throw new Error('Invalid argument: password');
     if(!args.email) throw new Error('Invalid argument: email');
-    if(!args.permission_id) args.permission_id =2;
+    if(!args.permission_id) args.permission_id = 2;
+    const permission = await _ValidatePermissionId(args.permission_id);
+    if(!permission) throw new Error('Invalid argument: permission_id not found')
     const user = await db.user.create({
         first_name:args.first_name,
         last_name:args.last_name,
@@ -48,8 +71,23 @@ async function CreateUser(args){
     });
     return user;
 }
+//private function starts with uderscore _ sign,
+async function _ValidatePermissionId(permission_id){
+    const permissions = await db.permission.findOne({
+        where: {
+            id:permission_id,
+        },
+    });
+    if(permissions){
+        return permissions;
+    }
+    return null;
+}
+
+
 module.exports = {
     UsernameExists,
     EmailExists,
     CreateUser,
+    FindUserByUserId,
 }
