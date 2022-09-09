b2353b28d62f1b4ec2a3bc814f41508a55d10972
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-09 16:30:49 -0700

    Update User_Gateway.js

--- API_Gateways/User_Gateway.js
+++ API_Gateways/User_Gateway.js
@@ -4,49 +4,65 @@ const router = express.Router();
 const asyncHandler = require("../Helpers/asyncHandler");
 const validate = require("validate.js");
 //services
-const User = require('../Services/Users/User_Service');
+const User = require("../Services/Users/User_Service");
 
-router.post('/', asyncHandler(async (req, res)=>{
-    const constraints ={
-        first_name:{
-            presence:true,
-            length:{maximum:50},
-        },
-        last_name:{
-            presence:true,
-            length:{maximum:50},
-        },
-        username:{
-            presence:true,
-            length:{minimum:8,maximum:20},
-        },
-        password:{
-            presence:true,
-            length:{minimum:8,maximum:20},
-        },
-        email:{
-            presence:true,
-            email:true,
-        },
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const constraints = {
+      first_name: {
+        presence: true,
+        length: { maximum: 50 },
+      },
+      last_name: {
+        presence: true,
+        length: { maximum: 50 },
+      },
+      username: {
+        presence: true,
+        length: { minimum: 8, maximum: 20 },
+      },
+      password: {
+        presence: true,
+        length: { minimum: 8, maximum: 20 },
+      },
+      email: {
+        presence: true,
+        email: true,
+      },
     };
-    const {first_name, last_name, username, password,email,permission_id} = req.body;
-    const validation = validate({first_name, last_name, username, password,email}, constraints);
-    if(validation){
-       return  res.status(400).json({error:validation});
+    const { first_name, last_name, username, password, email, permission_id } =
+      req.body;
+    const validation = validate(
+      { first_name, last_name, username, password, email },
+      constraints
+    );
+    if (validation) {
+      return res.status(400).json({ error: validation });
     }
     //forward user service
     const found_user = await User.ValidateUserExists(username, email);
-    if(found_user) {
-        if(username===found_user.username){
-            return res.status(400).json({error: `Username ${username} is already taken`})
-        }
-        if(email===found_user.email){
-            return res.status(400).json({error: ` email ${email} is already taken`})
-        }
-
+    if (found_user) {
+      if (username === found_user.username) {
+        return res
+          .status(400)
+          .json({ error: `Username ${username} is already taken` });
+      }
+      if (email === found_user.email) {
+        return res
+          .status(400)
+          .json({ error: ` email ${email} is already taken` });
+      }
     }
-    const new_user =  await User.CreateNewUser({first_name, last_name, username, email, password, permission_id,});
-    res.status(200).send({new_user});
-}));
+    const new_user = await User.CreateNewUser({
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+      permission_id,
+    });
+    res.status(200).send({ new_user });
+  })
+);
 module.exports = router;
-
