d49355f9d37651def7affac01f35dec5539f1cec
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-09 16:31:25 -0700

    Update User_DB.js

--- Services/Users/User_DB.js
+++ Services/Users/User_DB.js
@@ -1,22 +1,21 @@
 const db = require("../../models");
-const {formatArgs} = require("debug");
+const { formatArgs } = require("debug");
 
 /*
 Functions checks if username already exists on database.
 Returns user if username already taken, false otherwise,
  */
-async  function UsernameExists(username){
-    if(username===null || username=== undefined){
-        throw new Error('No Username was passed as an argument')
-
-    }
-    const user = await db.user.findOne({
-        where:{username},
-    })
-    if(user){
-        return user;
-    }
-    return null;
+async function UsernameExists(username) {
+  if (username === null || username === undefined) {
+    throw new Error("No Username was passed as an argument");
+  }
+  const user = await db.user.findOne({
+    where: { username },
+  });
+  if (user) {
+    return user;
+  }
+  return null;
 }
 
 /*
@@ -25,69 +24,67 @@ async  function UsernameExists(username){
 -user_id:string,
 *
 * */
-async function FindUserByUserId(user_id){
-    if(!user_id) throw new Error("Invalid argument: user_id");
-    const user = await db.user.findOne({
-        where:{id:user_id},
-    })
-    if(user){
-        return user;
-    }
-    return null;
+async function FindUserByUserId(user_id) {
+  if (!user_id) throw new Error("Invalid argument: user_id");
+  const user = await db.user.findOne({
+    where: { id: user_id },
+  });
+  if (user) {
+    return user;
+  }
+  return null;
 }
 
 /*Function checks if email already exists in database
 returns user if email is already taken ,false otherwise,*/
-async function  EmailExists(email){
-    if(email===null || email=== undefined){
-        throw new Error('No Email  was passed as an argument')
-
-    }
-    const user = await db.user.findOne({
-        where:{email},
-    })
-    if(user){
-        return user;
-    }
-    return null;
+async function EmailExists(email) {
+  if (email === null || email === undefined) {
+    throw new Error("No Email  was passed as an argument");
+  }
+  const user = await db.user.findOne({
+    where: { email },
+  });
+  if (user) {
+    return user;
+  }
+  return null;
 }
 
-async function CreateUser(args){
-    if(!args.username) throw new Error('Invalid argument: username');
-    if(!args.first_name) throw new Error('Invalid argument: first_name');
-    if(!args.last_name) throw new Error('Invalid argument: last_name');
-    if(!args.password) throw new Error('Invalid argument: password');
-    if(!args.email) throw new Error('Invalid argument: email');
-    if(!args.permission_id) args.permission_id = 2;
-    const permission = await _ValidatePermissionId(args.permission_id);
-    if(!permission) throw new Error('Invalid argument: permission_id not found')
-    const user = await db.user.create({
-        first_name:args.first_name,
-        last_name:args.last_name,
-        username:args.username,
-        password:args.password,
-        email:args.email,
-        permission_id:args.permission_id,
-    });
-    return user;
+async function CreateUser(args) {
+  if (!args.username) throw new Error("Invalid argument: username");
+  if (!args.first_name) throw new Error("Invalid argument: first_name");
+  if (!args.last_name) throw new Error("Invalid argument: last_name");
+  if (!args.password) throw new Error("Invalid argument: password");
+  if (!args.email) throw new Error("Invalid argument: email");
+  if (!args.permission_id) args.permission_id = 2;
+  const permission = await _ValidatePermissionId(args.permission_id);
+  if (!permission) throw new Error("Invalid argument: permission_id not found");
+  const user = await db.user.create({
+    first_name: args.first_name,
+    last_name: args.last_name,
+    username: args.username,
+    password: args.password,
+    email: args.email,
+    permission_id: args.permission_id,
+  });
+  return user;
 }
 //private function starts with uderscore _ sign,
-async function _ValidatePermissionId(permission_id){
-    const permissions = await db.permission.findOne({
-        where: {
-            id:permission_id,
-        },
-    });
-    if(permissions){
-        return permissions;
-    }
-    return null;
+async function _ValidatePermissionId(permission_id) {
+  const permissions = await db.permission.findOne({
+    where: {
+      id: permission_id,
+    },
+  });
+  if (permissions) {
+    return permissions;
+  }
+  return null;
 }
-
 
 module.exports = {
-    UsernameExists,
-    EmailExists,
-    CreateUser,
-    FindUserByUserId,
-}
+  UsernameExists,
+  EmailExists,
+  CreateUser,
+  FindUserByUserId,
+};
