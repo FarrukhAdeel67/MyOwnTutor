8fc4c5fe1fdaecee8d1e955011039f20b6f8e9b9
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-09 16:31:54 -0700

    Update Permissions.js

--- models/Permissions.js
+++ models/Permissions.js
@@ -1,27 +1,30 @@
 "use strict";
-const {DataTypes} = require("sequelize");
-module.exports = (sequelize, DataTypes) =>{
-    return  sequelize.define('permission', {
-            id:{
-                type:DataTypes.INTEGER,
-                autoIncrement:true,
-                primaryKey:true,
-                allowNull:false,
-            },
-            type:{
-                type:DataTypes.STRING,
-                required:true,
-                allowNull: false,
-            },
-            updated_at:{
-                type:DataTypes.DATE,
-            },
-            deleted_at:{
-                type:DataTypes.DATE,
-            },
-        },
-        {
-            underscored:true,
-            paranoid:true,
-        });
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "permission",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
+      deleted_at: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      underscored: true,
+      paranoid: true,
+    }
+  );
 };
