59ba7a56c21ef7c5ccd173bb475c4501b8800d6d
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-09 16:32:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const createError = require('http-errors');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const passport = require('passport');
 
 const app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Passport authentication.
+app.use(passport.initialize());
+
 //application Gateways
-app.use('/user', require('./API_Gateways/User_Gateway'));
+app.use("/user", require("./API_Gateways/User_Gateway"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
