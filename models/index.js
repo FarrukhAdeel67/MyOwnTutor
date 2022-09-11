2e500fc4d32f37af3e2ff940e3b4fd0c56e40b8d
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-10 22:12:59 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -6,9 +6,9 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const config = require("../config/config");
 const db = {};
-//console.log(config);
+ //console.log(config);
 // const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
-const sequelize = new Sequelize("myowntutor", "root", "pps993icp956", {
+const sequelize = new Sequelize("myowntutor", "root", "", {
   dialect: "mysql",
   host: "localhost",
 });
